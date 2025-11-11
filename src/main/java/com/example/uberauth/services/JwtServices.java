7dc25ecc01189ea0ae07169369a1956beed6a70e
Sanskar-1singh <sanskarsingh812@gmail.com>
2025-11-11 12:02:05 +0530

    Implemented jwt signin api

--- src/main/java/com/example/uberauth/services/JwtServices.java
+++ src/main/java/com/example/uberauth/services/JwtServices.java
@@ -1,6 +1,8 @@
 package com.example.uberauth.services;
 
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParserBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -8,11 +10,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtServices implements CommandLineRunner {
@@ -27,7 +31,7 @@ public class JwtServices implements CommandLineRunner {
      * it will create brand new  jwt token based on payload>>>
      * here we have taken map because JSon objct can be analogy w.r.t as map data structures>>>
      */
-    private String createToken(Map<String,Object> payload,String username){
+    private String createToken(Map<String,Object> payload,String email){
 
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + expiry);
@@ -35,16 +39,64 @@ public class JwtServices implements CommandLineRunner {
         Key key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
 
          return Jwts.builder()
-                 .claims(payload)
-                 .issuedAt(new Date(System.currentTimeMillis()))
-                 .expiration(expiryDate)
-                 .subject(username)
-                 .signWith(key)
+                 .setClaims(payload)
+                 .setSubject(email)
+                 .setIssuedAt(now)
+                 .setExpiration(expiryDate)
+                 .signWith(key, SignatureAlgorithm.HS256)
                  .compact();
                  
     }
 
-    @Override
+    public Claims extractPayload(String token){
+        Key key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+           return  Jwts.parserBuilder()
+                   .setSigningKey(key)
+                   .build()
+                   .parseClaimsJws(token)
+                   .getBody();
+    }
+
+   public <T> T extractClaims(String token, Function<Claims, T> claimsResolver){
+        final Claims claims=extractPayload(token);
+        return claimsResolver.apply(claims);
+   }
+
+
+    private Date extractExpiration(String token) {
+       return extractClaims(token, Claims::getExpiration);
+
+    }
+
+    private Boolean isTokenExpired(String token){
+        //this method check if token expiry is before current timestamp>>>
+        //true if token is expire or false it not expired>>>
+        return extractExpiration(token).before(new Date());
+      }
+
+      private String extractEmail(String token) {
+        return extractClaims(token, Claims::getSubject);
+      }
+
+
+      private Boolean validateToken(String token,String email) {
+        final String userEmail=extractEmail(token);
+         return (userEmail.equals(email)) &&  (!isTokenExpired(token));
+      }
+
+      public String getPhoneNumber(String token){
+        Claims claim=extractPayload(token);
+        String number=(String) claim.get("phoneNumber");
+        return number;
+      }
+
+      private String extractKeyPayload(String token,String key){
+        Claims claims=extractPayload(token);
+        return (String)claims.get(key);
+      }
+
+
+    @Override//it is just for extra information retentiuon policy is compile time
     public void run(String... args) throws Exception {
 
          Map<String,Object> payload = new HashMap<>();
@@ -52,6 +104,8 @@ public class JwtServices implements CommandLineRunner {
          payload.put("password","Sanskar@123");
         String token=createToken(payload,"Sanskar singh");
         System.out.println(token);
+        String x=extractKeyPayload(token,"phoneNumber");
+        System.out.println(x);
 
     }
 }
